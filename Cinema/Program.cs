712b57ede901b8e841572696d7df1973c6feef41
5202616 <5202616@Q035486>
2019-12-17 12:31:40 +0000

    Add project files.

---
+++ Cinema/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cinema
+{
+class Program
+{
+static void Main(string[] args)
+{
+            string login;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Gloucester Cinema | \n------------------|");
+            Console.WriteLine();
+            Console.WriteLine("If you're an existing user, log in, and if you're a new user, register");
+            Console.Write("Login:");
+            Console.SetCursorPosition(6, 4);
+            login = Console.ReadLine();
+
+        }//main args
+}//program
+}//Namespace
+    
