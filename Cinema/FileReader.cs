e7d1752dda55608c9f912e3b2af9b84ad5609d64
LENOVO <changenickdude@gmail.com>
2019-12-19 13:02:30 +0000

    stream writer and reader classes

---
+++ Cinema/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Cinema
+{
+    class FileReader
+    {
+        try
+            {
+                StreamReader read = new StreamReader(@"C:\Users\LENOVO\source\repos\ChangeNick420\Cinema");
+        line = read.ReadLine();
+                while (line != null)
+                {
+                    line = read.ReadLine();
+                }
+    read.Close();
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
+    }
+}
--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -6,55 +6,81 @@ class Program
 {
         static void Main(string[] args)
         {
-            string login;
-            string account;
-            string password;
-            string rlogin;
-            string rpassword;
+            string login="";
+            string account="";
+            string password="";
+            string rlogin="";
+            string rpassword="";
             string name = "";    
-            int age;
+            int age=0;
             int i = 0;
-            //StreamReader read = new StreamReader(@"C:\Users\5202616\source\repos\ChangeNick420\Cinema");
-            string FilePath = @"C:\Users\5202616\source\repos\ChangeNick420\Cinema";
-            string test=File.ReadAllText(@"C:\Users\5202616\source\repos\ChangeNick420\Cinema");
+            string line = "";
             
-            foreach (string in line in line)
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Welcome to Gloucester Cinema.| \n________________________________________________________|");
-            Console.WriteLine();
-            do
+            
+            try
             {
-                Console.WriteLine("Have you got an existing account? Yes/No");
-                account = Console.ReadLine().ToUpper();
-                if (account == "YES")
-                {
-                    Console.Write("Login:");
-                    login = Console.ReadLine();
-                    Console.Write("Password");
-                    password = Console.ReadLine();
-                    i++;
-                }
-                else if (account == "NO")
-                {
-                    Console.Write("Create Login:");
-                    login = Console.ReadLine();
-                    Console.Write("Create Password:");
-                    password = Console.ReadLine();
-                    Console.Write("Name:");
-                    name = Console.ReadLine();
-                    Console.Write("Age:");
-                    age = Convert.ToInt32(Console.ReadLine());
-                    i++;
+                StreamWriter write = new StreamWriter(@"C:\Users\LENOVO\source\repos\ChangeNick420\Cinema"); line = read.ReadLine();
+                
+                    write.WriteLine(rlogin);
+                write.WriteLine(rpassword);
+                write.WriteLine(name);
+
                     
+                
+                write.Close();
+            }
+            catch (Exception e)
+            {
 
+            }
+            finally
+            {
 
-                    File.WriteAllText(FilePath, login, password, );
+            }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Welcome to Gloucester Cinema.| \n________________________________________________________|");
+                Console.WriteLine();
+                do
+                {
+                    Console.WriteLine("Have you got an existing account? Yes/No");
+                    account = Console.ReadLine().ToUpper();
+                    if (account == "YES")
+                    {
+                        Console.Write("Login:");
+                        login = Console.ReadLine();
+                        Console.Write("Password");
+                        password = Console.ReadLine();
+                        i++;
+                        if (login == line && password == line)
+                        {
+                            Console.WriteLine("Access Granted");
+                        }
+                        else if (login != line || password != line)
+                        {
+                            Console.WriteLine("Access Denied. Try again");
+                        }
+                    }
+                    else if (account == "NO")
+                    {
+                        Console.Write("Create Login:");
+                        login = Console.ReadLine();
+                        Console.Write("Create Password:");
+                        password = Console.ReadLine();
+                        Console.Write("Name:");
+                        name = Console.ReadLine();
+                        Console.Write("Age:");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        i++;
+                    }
                 }
-            } while (i == 0);
-            Console.WriteLine("Hello {0}", name);
-            Console.ReadLine();
-            
-        }//main args
+                while (i == 0);
+
+
+                    Console.WriteLine("Hello {0}", name);
+                    Console.ReadLine();
+                
+                
+                }  //main args
 }//program
 }//Namespace
-    
